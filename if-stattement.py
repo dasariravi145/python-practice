43dfbbac0de86c9d4daf9ba1b1210e60327b6481
unknown <dasariravid145@gmail.com>
2026-09-22 19:37:49 +0530

    python

---
+++ if-stattement.py
@@ -0,0 +1,19 @@
+age = 17
+
+if (age > 18):
+    print("you can drive")
+    print("thank you")
+
+print("End of the program")
+
+age = int(input("enter the age"))
+
+if(age>18):
+
+      print("you can drive")
+else:
+      print("you can't drive")
+
+
+print("end of this is for input fiedls")
+
