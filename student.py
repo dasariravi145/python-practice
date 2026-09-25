ecbf2afb118f4fc653b15c69c7f9a8b08d4b66e4
unknown <dasariravid145@gmail.com>
2026-09-25 23:32:34 +0530

    python

---
+++ student.py
@@ -0,0 +1,9 @@
+def student(sub1,sub2,sub3,school="z.p.high",contry="india"):
+      print("sub1=",sub1)
+      print("sub2=",sub2)
+      print("sub3=",sub3)
+      print("school=",school)
+      print("contry=",contry)
+      print("total Marks=", sub1+sub2+sub3)
+
+student(23,45,65,contry="UK")
